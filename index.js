585ef88411d96628147fee16cb0ff3a86243cc48
Enver <mr_epsty@hotmail.com>
2023-09-16 17:43:17 +1000

    objects

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAdress: "11 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, ...{[key]: value}};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObject = {...employee,}
+    console.log(newObject)
+    delete newObject[key]
+    return newObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key]
+    return object
+}
